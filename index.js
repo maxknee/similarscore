1da4c270f1cb81f5e783b342446bdc6953cd3d65
maxknee <maxknee@gmail.com>
2016-01-18 21:30:58 -0500

    Adding parser

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 var fs = require('fs');
-var csv = require('csv-parser');
+var csv = require('csv');
+var parse = require('csv-parse');
+var transform = require('stream-transform');
 
+require('should');
+var output = [];
 
-fs.createReadStream('orders.csv')
-    .pipe(csv())
-    .on('data', function(data) {
-        console.log('row', data);    
-    });
+function Order(product, customer) {
+  this.Product = product;
+  this.Customer = customer;
+}
+
+var Orders = [];
+
+var parser = csv.parse({}, function(err, data){
+  output.push(data);
+  transformer.write(data);
+  for (var i = 0; i < data.length; i++) {
+    Orders.push(data[i]);
+    console.log(Orders[i]);
+  };
+});
+
+var transformer = csv.transform(function(data){
+  return data.map;
+});
+
+transformer.on('readable', function() {
+  while(row = transformer.read()) {
+    output.push(row);
+  }
+  console.log('this output');
+  console.log(output);
+});
+fs.createReadStream('orders.csv').pipe(transformer).pipe(parser);

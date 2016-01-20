d565b93e55d63b89711d80a366aed029ae60bec9
Max Knee <max@moven.com>
2016-01-20 17:20:10 -0500

    Adding comments, finalizing, and adding Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Similar Score alogrithm.
+
+## Node js program to find the score
+### In order for me to find out I took the scores and ran them against if they were part of same purchases from same customers or if similar items were bought by same customers
+
+To run, type `npm install` then `node index.js`
--- index.js
+++ index.js
@@ -6,35 +6,104 @@ var transform = require('stream-transform');
 require('should');
 var output = [];
 var counts = {};
-
-function Order(product, customer) {
-  this.Product = product;
-  this.Customer = customer;
-}
-
+var similarProd = [];
 var Orders = [];
+var similarOrders= [];
+var similarScore = {};
+
+//
+// Parsing csv into array 
+//
 
 var parser = csv.parse({}, function(err, data){
   output.push(data);
   for (var i = 0; i < data.length; i++) {
-    Orders.push(new Order(data[i][0], data[i][1]));
+    Orders.push({Product: Number(data[i][0]), Customer: data[i][1]});
   };
+  
+  //
+  // Removing csv headers since I created objects for easier naming.
+  //
+
   Orders.shift();
-  console.log(Orders[3].Product);
-  /*
-  Orders.forEach(function(obj) {
-      var key = JSON.stringify(obj);
-      counts[key] = (counts[key] || 0) + 1;
-  });
-  */
+
+  //
+  // Counting to see how many times products were ordered.
+  //
+
   for (var d = 0; d < Orders.length; d++) {
-      var hey = Orders[d].Product
-   counts[hey] = (counts[hey] || 0) + 1;
+    var prodCount = Orders[d].Product;
+    counts[prodCount] = (counts[prodCount] || 0) + 1;
   }
-  console.log(counts);
+
+
+  //
+  // Grouping products into if they were bought more than once
+  //
+
+  for (var key in counts){
+    if(counts.hasOwnProperty(key)){
+      if (counts[key] > 1) {
+         for (var i = 0, l = Orders.length; i < l; i ++) {
+             var v = Orders[i];
+             if (key == v.Product) {
+               similarProd.push(v);
+            }
+          }
+       }
+    }
+  }
+
+  //
+  // Comparing to see if products were bought by the same customers
+  // And adding to new array to compare to.
+  //
+
+  for (var i = 0; i < similarProd.length -1;) {
+    var firstValue = similarProd[i];
+    var secondValue = similarProd[i + 1];
+    
+    if(firstValue.Product == secondValue.Product) {
+      for (var x = 0; x < similarProd.length;) {
+        if (firstValue == similarProd[x]) {
+          similarOrders.push(similarProd[i]);
+          x++;
+        }
+        else {
+          x++;
+        }
+      }
+    };
+    i++;
+  };
+
+  //
+  // If products were more ordered than others and by
+  // Same customers then given higher score since they
+  // Would show up more than other products
+  // Adding to object with score.
+  //
+
+  for (var i = 0; i < similarOrders.length -1; i++) {
+    var f = 0;
+    for(var x = 0; x < similarOrders.length;){
+      var firstCustomer = similarOrders[i];
+      var secondCustomer = similarOrders[x];
+      if(firstCustomer.Customer == secondCustomer.Customer){
+        similarScore[firstCustomer.Product] = (similarScore[firstCustomer.Product] || 0) + 1;
+        x++;
+      }
+      else if (firstCustomer.Product == similarOrders[x].Product) {
+        similarScore
+        x++;
+      }
+      else {
+        x++;
+      }
+    };
+  };
+  
+  console.log(similarScore);
 });
 
 fs.createReadStream('orders.csv').pipe(parser);
-
-
-console.log(counts);
